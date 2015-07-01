1e308964b6478fd6d9b0f47fbd5fcf7951d3611a
tansongyang <tansongyang@gmail.com>
2015-07-01 14:46:13 -0400

    Reorganize index.js.

--- scripts/index.js
+++ scripts/index.js
@@ -1,44 +1,51 @@
 (function(){
-	document.addEventListener("DOMContentLoaded", function () {
-		var sampleCode = "function Foo(id) { this.id = id };" +
+	var sampleCode = "function Foo(id) { this.id = id };" +
 		"var foo = new Foo('foo');" +
 		"var bar = Object.create(foo);" +
 		"bar.id = 'bar';" +
 		"exports.object = bar;" +
 		"exports.constructors = [Foo];";
-		var codeArea = document.getElementById("code");
+	var codeArea;
+
+	document.addEventListener("DOMContentLoaded", function () {
+		codeArea = document.getElementById("code");
 
 		document.getElementById("run").addEventListener("click", function () {
-			var code = new Function("exports", codeArea.value || sampleCode);
-			var exports = {};
-			code(exports);
+			var exports = parseJS(codeArea.value || sampleCode);
 			display(exports);
 		});
+	});
 
-		function display(exports) {
-			var message = "The id of the object is: " + exports.object.id + "\r\n";
-			var prototype, constructor;
+	function parseJS(text) {
+		var code = new Function("exports", text);
+		var exports = {};
+		code(exports);
+		return exports;
+	}
 
-			for (prototype = Object.getPrototypeOf(exports.object); prototype; prototype = Object.getPrototypeOf(prototype)) {
-				if (prototype.id) {
-					message += "Its prototype is the object: " + prototype.id + "\r\n";
-					continue;
-				}
+	function display(exports) {
+		var message = "The id of the object is: " + exports.object.id + "\r\n";
+		var prototype, constructor;
 
-				constructor = exports.constructors.filter(function(element) {
-					return prototype === element.prototype;
-				})[0];
-				if (constructor) {
-					message += "Its prototype is the prototype of the constructor function: " + constructor + "\r\n";
-					continue;
-				}
-
-				if (prototype === Object.prototype) {
-					message += "This is the end of the chain: Object.prototype.\r\n";
-				}
+		for (prototype = Object.getPrototypeOf(exports.object); prototype; prototype = Object.getPrototypeOf(prototype)) {
+			if (prototype.id) {
+				message += "Its prototype is the object: " + prototype.id + "\r\n";
+				continue;
 			}
 
-			alert(message);
+			constructor = exports.constructors.filter(function(element) {
+				return prototype === element.prototype;
+			})[0];
+			if (constructor) {
+				message += "Its prototype is the prototype of the constructor function: " + constructor + "\r\n";
+				continue;
+			}
+
+			if (prototype === Object.prototype) {
+				message += "This is the end of the chain: Object.prototype.\r\n";
+			}
 		}
-	});
+
+		alert(message);
+	}
 })();
