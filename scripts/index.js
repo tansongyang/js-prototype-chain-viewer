8ebc76c4c6d784d11548888df53360136a0ca1c0
tansongyang <tansongyang@gmail.com>
2015-07-01 17:03:18 -0400

    Output prototype chain as divs.

--- scripts/index.js
+++ scripts/index.js
@@ -1,51 +1,85 @@
-(function(){
+(function() {
+	"use strict";
+
 	var sampleCode = "function Foo(id) { this.id = id };" +
 		"var foo = new Foo('foo');" +
 		"var bar = Object.create(foo);" +
 		"bar.id = 'bar';" +
 		"exports.object = bar;" +
 		"exports.constructors = [Foo];";
-	var codeArea;
+	var codeArea, output;
 
-	document.addEventListener("DOMContentLoaded", function () {
+	document.addEventListener("DOMContentLoaded", function() {
 		codeArea = document.getElementById("code");
+		output = document.getElementById("output");
 
-		document.getElementById("run").addEventListener("click", function () {
+		document.getElementById("run").addEventListener("click", function() {
 			var exports = parseJS(codeArea.value || sampleCode);
 			display(exports);
 		});
 	});
 
 	function parseJS(text) {
+		/* jslint evil: true*/
+		// This is part of required functionality
 		var code = new Function("exports", text);
 		var exports = {};
-		code(exports);
+		code(exports); // Assume users will put base object in exports.object and constructors in exports.constructors
 		return exports;
 	}
 
 	function display(exports) {
-		var message = "The id of the object is: " + exports.object.id + "\r\n";
-		var prototype, constructor;
+		var div, prototype, constructor;
 
+		// Predicate for checking
+		function prototypeIsContructorPrototype(constructor) {
+			return prototype === constructor.prototype;
+		}
+
+		// Clear original
+		while (output.firstChild) {
+			output.removeChild(output.firstChild);
+		}
+
+		// Append base object
+		div = document.createElement("div");
+		div.id = "object";
+		div.innerHTML = exports.object.id;
+		output.appendChild(div);
+
+		// Walk prototype chain
 		for (prototype = Object.getPrototypeOf(exports.object); prototype; prototype = Object.getPrototypeOf(prototype)) {
 			if (prototype.id) {
-				message += "Its prototype is the object: " + prototype.id + "\r\n";
+				// For now, assume users will give objects an id attribute
+				div = document.createElement("div");
+				div.classList.add("prototype-object");
+				div.innerHTML = prototype.id;
+				output.appendChild(div);
 				continue;
 			}
 
-			constructor = exports.constructors.filter(function(element) {
-				return prototype === element.prototype;
-			})[0];
+			constructor = exports.constructors.filter(prototypeIsContructorPrototype)[0];
 			if (constructor) {
-				message += "Its prototype is the prototype of the constructor function: " + constructor + "\r\n";
+				div = document.createElement("div");
+				div.classList.add("constructor-prototype");
+				div.classList.add("constructor");
+				div.innerHTML = "(" + constructor + ").prototype";
+				output.appendChild(div);
+
+				div = document.createElement("div");
+				div.classList.add("constructor");
+				div.innerHTML = constructor;
+				output.appendChild(div);
+
 				continue;
 			}
 
 			if (prototype === Object.prototype) {
-				message += "This is the end of the chain: Object.prototype.\r\n";
+				div = document.createElement("div");
+				div.id = "object-prototype";
+				div.innerHTML = "Object.prototype";
+				output.appendChild(div);
 			}
 		}
-
-		alert(message);
 	}
 })();
