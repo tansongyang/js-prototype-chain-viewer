d7b986e5019aff6a48e5285e899316ce588fb49c
tansongyang <tansongyang@gmail.com>
2015-07-12 17:27:23 -0400

    Use browserify + babelify. Add package.json. Build with "npm run build-debug". Replace anonymous functions in index.js with arrow functions.

--- scripts/index.js
+++ scripts/index.js
@@ -1,134 +1,132 @@
-(function() {
-	"use strict";
+"use strict";
 
-	var sampleCode = "function Foo(id) { this.id = id };" +
-		"\r\nvar foo = new Foo('foo');" +
-		"\r\nvar bar = Object.create(foo);" +
-		"\r\nbar.id = 'bar';" +
-		"\r\nexports.object = bar;" +
-		"\r\nexports.constructors = [Foo];";
-	var codeArea, outputContent;
+var sampleCode = "function Foo(id) { this.id = id };" +
+	"\r\nvar foo = new Foo('foo');" +
+	"\r\nvar bar = Object.create(foo);" +
+	"\r\nbar.id = 'bar';" +
+	"\r\nexports.object = bar;" +
+	"\r\nexports.constructors = [Foo];";
+var codeArea, outputContent;
 
-	document.addEventListener("DOMContentLoaded", function() {
-		codeArea = document.getElementById("code");
-		outputContent = document.getElementById("output-content");
+document.addEventListener("DOMContentLoaded", () => {
+	codeArea = document.getElementById("code");
+	outputContent = document.getElementById("output-content");
 
-		document.getElementById("run").addEventListener("click", function() {
-			codeArea.value = codeArea.value || sampleCode;
-			var exports = parseJS(codeArea.value || sampleCode);
+	document.getElementById("run").addEventListener("click", () => {
+		codeArea.value = codeArea.value || sampleCode;
+		var exports = parseJS(codeArea.value || sampleCode);
 
-			exports.constructors = exports.constructors || [];
-			exports.constructors = exports.constructors.concat([
-				Function
-			]); // Include fundamental objects
+		exports.constructors = exports.constructors || [];
+		exports.constructors = exports.constructors.concat([
+			Function
+		]); // Include fundamental objects
 
-			display(exports);
-		});
-
-		document.getElementById("code-wrapper").classList.remove("is-uninitialized");
+		display(exports);
 	});
 
-	function parseJS(text) {
-		/* jslint evil: true*/
-		// This is part of required functionality
-		var code = new Function("exports", text);
-		var exports = {};
-		code(exports); // Assume users will put base object in exports.object and constructors in exports.constructors
-		return exports;
+	document.getElementById("code-wrapper").classList.remove("is-uninitialized");
+});
+
+function parseJS(text) {
+	/* jslint evil: true*/
+	// This is part of required functionality
+	var code = new Function("exports", text);
+	var exports = {};
+	code(exports); // Assume users will put base object in exports.object and constructors in exports.constructors
+	return exports;
+}
+
+function display(exports) {
+	var prototype, constructor, nodes, i;
+
+	// Predicate for checking if prototype is a constructor prototype
+	function prototypeIsContructorPrototype(constructor) {
+		return prototype === constructor.prototype;
 	}
 
-	function display(exports) {
-		var prototype, constructor, nodes, i;
-
-		// Predicate for checking if prototype is a constructor prototype
-		function prototypeIsContructorPrototype(constructor) {
-			return prototype === constructor.prototype;
-		}
-
-		// Clear original
-		while (outputContent.firstChild) {
-			outputContent.removeChild(outputContent.firstChild);
-		}
-
-		// Append root object
-		appendPrototypeNode(exports.object.id, ["prototype"], "root-object");
-
-		// Walk prototype chain
-		for (prototype = Object.getPrototypeOf(exports.object); prototype; prototype = Object.getPrototypeOf(prototype)) {
-			if (prototype === Object.prototype) {
-				// Reached the end, which is Object.prototype
-				appendPrototypeNode("Object.prototype", ["prototype"], "object-prototype");
-				break;
-			}
-
-			constructor = exports.constructors.filter(prototypeIsContructorPrototype)[0];
-			if (constructor) {
-				// This prototype is a constructor's prototype
-				appendPrototypeNode(constructor.name, ["constructor"], constructor.name, true);
-			} else {
-				// This prototype is just an object
-				appendPrototypeNode(prototype.id, ["prototype"]);
-			}
-		}
-
-		// Display all nodes; temporary hack using setTimeout and hardcoded delay
-		nodes = document.querySelectorAll(".output-div");
-		i = 0;
-		setTimeout(function removeHidden() {
-			if (i < nodes.length) {
-				nodes[i].classList.remove("is-hidden");
-				i++;
-				setTimeout(removeHidden, 200);
-			}
-		}, 200);
+	// Clear original
+	while (outputContent.firstChild) {
+		outputContent.removeChild(outputContent.firstChild);
 	}
 
-	function appendPrototypeNode(text, classList, id, isConstructor) {
-		var div = document.createElement("div"),
-			innerDiv;
+	// Append root object
+	appendPrototypeNode(exports.object.id, ["prototype"], "root-object");
 
-		if (isConstructor) {
-			// Group constructor and constructor,prototype together
-			div.classList.add("constructor-prototype-wrapper");
+	// Walk prototype chain
+	for (prototype = Object.getPrototypeOf(exports.object); prototype; prototype = Object.getPrototypeOf(prototype)) {
+		if (prototype === Object.prototype) {
+			// Reached the end, which is Object.prototype
+			appendPrototypeNode("Object.prototype", ["prototype"], "object-prototype");
+			break;
+		}
 
-			innerDiv = getPrototypeNode(id + ".prototype", ["prototype", "constructor-prototype"]);
-			div.appendChild(innerDiv);
-
-			innerDiv = getPrototypeNode(id, ["constructor"]);
-			div.appendChild(innerDiv);
+		constructor = exports.constructors.filter(prototypeIsContructorPrototype)[0];
+		if (constructor) {
+			// This prototype is a constructor's prototype
+			appendPrototypeNode(constructor.name, ["constructor"], constructor.name, true);
 		} else {
-			div = getPrototypeNode(text, classList, id);
+			// This prototype is just an object
+			appendPrototypeNode(prototype.id, ["prototype"]);
 		}
-
-		outputContent.appendChild(div);
 	}
 
-	function getPrototypeNode(text, classList, id) {
-		var div = document.createElement("div");
-		var i;
-
-		if (text) {
-			div.innerHTML = '<span class="output-div-text">' + text + "</span>";
+	// Display all nodes; temporary hack using setTimeout and hardcoded delay
+	nodes = document.querySelectorAll(".output-div");
+	i = 0;
+	setTimeout(function removeHidden() {
+		if (i < nodes.length) {
+			nodes[i].classList.remove("is-hidden");
+			i++;
+			setTimeout(removeHidden, 200);
 		}
-		div.classList.add("output-div");
-		div.classList.add("is-hidden");
-		if (classList && classList.length >= 0) {
-			for (i = 0; i < classList.length; i++) {
-				div.classList.add(classList[i]);
-			}
-		}
-		if (id) {
-			div.id = id;
-		}
+	}, 200);
+}
 
-		div.addEventListener("click", function() {
-			if (div.classList.contains("is-active")) {
-				div.classList.remove("is-active");
-			} else {
-				div.classList.add("is-active");
-			}
-		});
+function appendPrototypeNode(text, classList, id, isConstructor) {
+	var div = document.createElement("div"),
+		innerDiv;
 
-		return div;
+	if (isConstructor) {
+		// Group constructor and constructor,prototype together
+		div.classList.add("constructor-prototype-wrapper");
+
+		innerDiv = getPrototypeNode(id + ".prototype", ["prototype", "constructor-prototype"]);
+		div.appendChild(innerDiv);
+
+		innerDiv = getPrototypeNode(id, ["constructor"]);
+		div.appendChild(innerDiv);
+	} else {
+		div = getPrototypeNode(text, classList, id);
 	}
-})();
+
+	outputContent.appendChild(div);
+}
+
+function getPrototypeNode(text, classList, id) {
+	var div = document.createElement("div");
+	var i;
+
+	if (text) {
+		div.innerHTML = '<span class="output-div-text">' + text + "</span>";
+	}
+	div.classList.add("output-div");
+	div.classList.add("is-hidden");
+	if (classList && classList.length >= 0) {
+		for (i = 0; i < classList.length; i++) {
+			div.classList.add(classList[i]);
+		}
+	}
+	if (id) {
+		div.id = id;
+	}
+
+	div.addEventListener("click", () => {
+		if (div.classList.contains("is-active")) {
+			div.classList.remove("is-active");
+		} else {
+			div.classList.add("is-active");
+		}
+	});
+
+	return div;
+}
