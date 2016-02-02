a760d7731a92993616f8c8fd06bcf9f0acbc5ffe
Frank Tan <tansongyang@gmail.com>
2016-02-01 20:48:40 -0500

    Use single quotes in PrototypeLink*.js.

--- test/PrototypeLinkTest.js
+++ test/PrototypeLinkTest.js
@@ -1,20 +1,19 @@
-"use strict";
+'use strict';
 
-import PrototypeLink from "../lib/PrototypeLink";
+import PrototypeLink from '../lib/PrototypeLink';
 
-describe("PrototypeLink", () => {
-  describe("constructor(object)", () => {
-    it("returns the same instance when created with the same object", () => {
-
+describe('PrototypeLink', () => {
+  describe('constructor(object)', () => {
+    it('returns the same instance when created with the same object', () => {
       let link0 = new PrototypeLink(Object.prototype);
       let link1 = new PrototypeLink(Object.prototype);
       expect(link0).to.equal(link1);
     });
   });
 
-  describe("#name()", () => {
+  describe('#name', () => {
     it('returns the "name" property of object', () => {
-      let name = "Carl Ditters von Dittersdorf";
+      let name = 'Carl Ditters von Dittersdorf';
       let object = {
         name: name
       };
@@ -25,26 +24,25 @@ describe("PrototypeLink", () => {
     it('returns "(anonymous)" when object has no "name" property', () => {
       let object = {};
       let link = new PrototypeLink(object);
-      expect(link.name).to.equal("(anonymous)");
+      expect(link.name).to.equal('(anonymous)');
     });
 
-    it("returns function name when object is a named function", () => {
+    it('returns function name when object is a named function', () => {
       function Ditters() {}
       let link = new PrototypeLink(Ditters);
-      expect(link.name).to.equal("Ditters");
+      expect(link.name).to.equal('Ditters');
 
       let f = function vonDittersdorf() {};
       link = new PrototypeLink(f);
-      expect(link.name).to.equal("vonDittersdorf");
+      expect(link.name).to.equal('vonDittersdorf');
     });
 
     it('returns "Constructor.prototype" when object is the prototype of the constructor function "Constructor"', () => {
-
       function Constructor() {}
       let c = new Constructor();
       let object = Object.getPrototypeOf(c);
       let link = new PrototypeLink(object);
-      expect(link.name).to.equal("Constructor.prototype");
+      expect(link.name).to.equal('Constructor.prototype');
     });
   });
 });
